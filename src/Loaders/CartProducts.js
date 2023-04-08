3aa7bbb98514da15865ef04075c0feb84f7fea6d
HmSani6699 <shahinur04042002@gmail.com>
2023-04-09 00:27:15 +0600

    added save card

--- src/Loaders/CartProducts.js
+++ src/Loaders/CartProducts.js
@@ -7,15 +7,19 @@ const cartProductLoader = async () => {
 
   //   if cart data is in database, you have to async await
   const storedCart = getShoppingCart();
+  const savedCart=[];
+
   console.log(storedCart);
   for (const id in storedCart) {
     const addedProduct = products.find((pd) => pd.id === id);
     if (addedProduct) {
       const quantity = storedCart[id];
       addedProduct.quantity = quantity;
+      savedCart.push(addedProduct);
     }
-  }
+  };
+  // if ou want to return two thing[]/{}
 
-  return products;
+  return savedCart;
 };
 export default cartProductLoader;
