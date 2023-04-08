b0ee8c81e2f3f8a6c423db2e43b3e4feffa79562
HmSani6699 <shahinur04042002@gmail.com>
2023-04-09 00:02:47 +0600

    added

--- src/Loaders/CartProducts.js
+++ src/Loaders/CartProducts.js
@@ -1,13 +1,21 @@
+import Product from "../components/Product/Product";
 import { getShoppingCart } from "../utilities/fakedb";
 
 const cartProductLoader = async () => {
   const loadedProducts = await fetch("products.json");
   const products = await loadedProducts.json();
 
+  //   if cart data is in database, you have to async await
+  const storedCart = getShoppingCart();
+  console.log(storedCart);
+  for (const id in storedCart) {
+    const addedProduct = products.find((pd) => pd.id === id);
+    if (addedProduct) {
+      const quantity = storedCart[id];
+      addedProduct.quantity = quantity;
+    }
+  }
 
-//   if cart data is in database, you have to async await
-const storedCart=getShoppingCart();
- console.log(storedCart)
   return products;
 };
 export default cartProductLoader;
