78c58bd35602740edf6aa0860d83b19ae3939610
HmSani6699 <shahinur04042002@gmail.com>
2023-04-09 12:14:21 +0600

    detel button

--- src/components/Orders/Order.jsx
+++ src/components/Orders/Order.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Cart from "../Cart/Cart";
 import { useLoaderData } from "react-router-dom";
 import cartProductLoader from "../../Loaders/CartProducts";
 import ReviewItem from "../RivewItem/ReviewItem";
 import './order.css'
+import { removeFromDb } from "../../utilities/fakedb";
 
 const Order = () => {
-    const cart=useLoaderData();
-    console.log(cart);
+    const savedCart=useLoaderData();
+    const [cart,setCart]=useState(savedCart);
+    
+    
+    const handleRemoveFromCart=(id)=>{
+      const reaming=cart.filter(product=>product.id !==id);
+      setCart(reaming);
+      removeFromDb(id)
+    }
+    // console.log(cart);
     // return products;
   return (
     <div className="shop-container">
       <div className="review-container">
         {
-          cart.map(product=><ReviewItem key={product.id} product={product}></ReviewItem>)
+          cart.map(product=><ReviewItem 
+            key={product.id} 
+            product={product}
+            handleRemoveFromCart={handleRemoveFromCart}></ReviewItem>)
         }
       </div>
       <div className="cart-container">
--- src/components/RivewItem/ReviewItem.jsx
+++ src/components/RivewItem/ReviewItem.jsx
@@ -4,23 +4,25 @@ import ReactDOM from "react-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-const ReviewItem = ({ product }) => {
+const ReviewItem = ({ product,handleRemoveFromCart }) => {
   const { id, name, price, img, quantity, shipping } = product;
   return (
     <div className="review-item">
-      <img src={img} alt="" />
-      <div className="twoThing">
+      <div className="review-info">
+        <img src={img} alt="" />
         <div>
           <p className="name">{name}</p>
           <p>
             Price:<span className="price">${price}</span>
           </p>
-          <p>Quantity:<span className="price">{quantity}</span> </p>
+          <p>
+            Quantity:<span className="price">{quantity}</span>{" "}
+          </p>
         </div>
-        <button className="btn-del">
+      </div>
+        <button onClick={()=>handleRemoveFromCart(id)} className="btn-del">
           <FontAwesomeIcon icon={faTrash} />
         </button>
-      </div>
     </div>
   );
 };
